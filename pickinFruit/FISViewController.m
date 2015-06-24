27822fb2dce87c1805edb38933f1991dedd7d7f8
lukasthoms <luk@stho.ms>
2015-06-24 17:24:00 -0400

    finished lab

--- pickinFruit/FISViewController.m
+++ pickinFruit/FISViewController.m
@@ -22,6 +22,9 @@
 	// Do any additional setup after loading the view, typically from a nib.
 
     self.fruitsArray = @[@"Apple",@"Orange",@"Banana",@"Pear",@"Grape", @"Kiwi", @"Mango", @"Blueberry", @"Raspberry"];
+//    self.fruitsArray = @[@"Apple", @"Orange"];
+    self.fruitPicker.delegate = self;
+    self.fruitPicker.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -30,9 +33,48 @@
     // Dispose of any resources that can be recreated.
 }
 
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 3;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.fruitsArray.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.fruitsArray[row];
+}
+
+
 
 - (IBAction)spin:(id)sender {
 
-#warning Implement this method!
+    for (NSInteger i = 0; i<3; i++) {
+        int randomRow = arc4random_uniform((u_int32_t)self.fruitsArray.count);
+        [self.fruitPicker selectRow:randomRow inComponent:i animated:YES];
+    }
+    [self checkForWin];
 }
+
+- (void) checkForWin {
+    UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"Cool." style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) { }];
+    if ([self.fruitPicker selectedRowInComponent:0] == [self.fruitPicker selectedRowInComponent:1] && [self.fruitPicker selectedRowInComponent:1] == [self.fruitPicker selectedRowInComponent:2]) {
+        UIAlertController *winAlert = [UIAlertController alertControllerWithTitle:@"YOU WIN" message:@"I CAN'T BELIEVE YOU WON" preferredStyle:UIAlertControllerStyleAlert];
+        
+        [winAlert addAction:okButton];
+        [self presentViewController:winAlert animated:YES completion:nil];
+    } else {
+        UIAlertController *loseAlert = [UIAlertController alertControllerWithTitle:@"Nah." message:@"You lost :(" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *reSpin = [UIAlertAction actionWithTitle:@"Respin" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            [self spin:nil];
+        }];
+        [loseAlert addAction:okButton];
+        [loseAlert addAction:reSpin];
+        [self presentViewController:loseAlert animated:YES completion:nil];
+        
+    }
+}
+
 @end
